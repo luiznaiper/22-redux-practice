4ac723daacb591356de622dad98813e30fb47825
luiznaiper <luiz_naiper@hotmail.com>
2022-08-01 16:13:41 -0500

    :tada: First mockup of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { Col } from 'antd';
+import { Searcher } from './components/Searcher';
 import './App.css';
+import { PokemonList } from './components/PokemonList';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">Prueba</div>;
+  return (
+    <div className="App">
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList />
+    </div>
+  );
 }
 
 export default App;
