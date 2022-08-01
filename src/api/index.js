0e17b51f231acb616cd70767d59d49b0b51384f4
luiznaiper <luiz_naiper@hotmail.com>
2022-08-01 16:47:47 -0500

    :construction: useEffect and axios to fetch pokemons

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const getPokemon = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=151'
+    );
+    console.log(data.results);
+    return data.results;
+  } catch (error) {
+    console.log('There was an error', error);
+  }
+};
+
+export { getPokemon };
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,7 +6,7 @@ const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
       {pokemons.map((pokemon) => {
-        return <PokemonCard />;
+        return <PokemonCard name={pokemon.name} key={pokemon.name} />;
       })}
     </div>
   );
