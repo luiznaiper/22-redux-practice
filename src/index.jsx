3fb05ee5efd451e3a4296fe099ec803f7d5b7c06
luiznaiper <luiz_naiper@hotmail.com>
2022-08-03 08:18:46 -0500

    :construction: Create rootReducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { pokemonsReducer } from './reducers/pokemons.js';
 import { Provider } from 'react-redux';
 import {
   applyMiddleware,
@@ -10,12 +9,13 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import { logger } from './middlewares';
+import rootReducer from './reducers/rootReducer';
 
 const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const composedEnhancers = compose(composeAlt(applyMiddleware(thunk, logger)));
 
-const store = createStore(pokemonsReducer, composedEnhancers);
+const store = createStore(rootReducer, composedEnhancers);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,9 +1,8 @@
 import { fromJS } from 'immutable';
-import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from '../actions/types';
+import { SET_FAVORITE, SET_POKEMONS } from '../actions/types';
 
 const initialState = fromJS({
   pokemons: [],
-  loading: false,
 });
 
 export const pokemonsReducer = (state = initialState, action) => {
@@ -11,9 +10,6 @@ export const pokemonsReducer = (state = initialState, action) => {
     case SET_POKEMONS:
       // return { ...state, pokemons: action.payload };
       return state.setIn(['pokemons'], fromJS(action.payload));
-
-    case SET_LOADING:
-      return state.setIn(['loading'], fromJS(action.payload));
 
     case SET_FAVORITE:
       const currentPokemonIndex = state.get('pokemons').findIndex((pokemon) => {
